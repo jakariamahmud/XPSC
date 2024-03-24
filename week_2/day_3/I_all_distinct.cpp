384ed3840bd501c8bd514b1f16d09f6c6dca2349
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-03-24 23:52:00 +0600

    all done

---
+++ week_2/day_3/I_all_distinct.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        for(int i=0;i<n;i=                    ki+2){
+                        .   000 
+        }.
+    }
+
+    return 0;
+}
