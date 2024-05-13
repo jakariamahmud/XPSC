a22d0f472b1846de818da9f6ec50116b9422ec7b
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-05-13 23:19:18 +0600

    all done

--- week_8/day_1/a.cpp
+++ week_8/day_1/a.cpp
@@ -1,1 +1,22 @@
-aa
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+      
+    int t;
+    cin>>t;
+    while(t--){
+        float l,a,b;
+        cin>>l>>a>>b;
+        int r1=ceil(l/a)-1,r2=ceil(l/b);
+        if(r1==r2) cout<<0<<endl;
+        else if(r1<r2) cout<<-1<<endl;
+        else cout<<r1-r2<<endl;
+
+    }
+      
+    return 0;
+}
