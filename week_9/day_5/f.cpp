5d33f65ed0cfd1335ab397842c983885294ae555
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-05-23 22:32:05 +0600

    all done

---
+++ week_9/day_5/f.cpp
@@ -0,0 +1,44 @@
+/*
+    ****************************************
+    *      Auther:  Jakaria Mahmud         *
+    *      Date: 24-04-2024 21:21:00       *
+    ****************************************
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+      
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long> v(n);
+        long long mn = LONG_MAX;
+
+        for(int i=0;i<n;i++) {
+            cin>>v[i];
+            mn = min(mn,v[i]);
+        }
+        sort(v.begin(),v.end());
+        long long ans= mn;
+        bool f=1;
+        for(int i=0;i<n;i++){
+            if(f && v[i]==mn) f=0;
+            else if(v[i]==1) ans+=v[i];
+            else{
+                ans*=v[i];
+            }
+        }
+        cout<<ans%100000000007<<endl;
+
+        
+    }
+      
+    return 0;
+}
