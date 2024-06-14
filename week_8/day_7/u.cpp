c707d2dd5b0c9ccd4eeff3df4fa1c48eb5be8594
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-06-14 12:30:35 +0600

    all done:

---
+++ week_8/day_7/u.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+      
+    int t;cin>>t;
+    while(t--){
+        long long n,sum=0;cin>>n;
+        vector<long long> v(n);
+        for(long long &x:v) {
+            cin>>x;
+            sum+=x;
+        }
+        
+    }
+      
+    return 0;
+}
