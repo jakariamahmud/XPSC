e4eaf0fb8c657c5ac725b9518253484f4712449b
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-06-22 14:18:13 +0600

    all done

---
+++ practice.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"jakaria mahmud"<<endl;
+    cout<<"amar sonar bangla ami tomay valobashi chirodin tomar akash tomay batash amar prane bazay bashi"<<endl;
+
+    return 0;
+}
