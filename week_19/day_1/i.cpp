926b8b264e3a2c7609c62e76ed32447a0f17ccff
Jakaria-Mahmud <95276970+jakariamahmud@users.noreply.github.com>
2024-09-02 23:48:42 +0600

    all done

---
+++ week_19/day_1/i.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+      
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vector<int> v(n);
+        for(int &x:v) cin>>x;
+        sort(v.begin(),v.end()-1);
+        cout<<v[n-2]+v[n-1]<<endl;
+    }
+      
+    return 0;
+}
